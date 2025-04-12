4915308dd6553235b82762e5db212e8a9623644d
m4tice <ductuan2908@gmail.com>
2025-04-12 13:50:22 +0700

    update: debug information

--- ComSclAppl/comsclappl_generator.py
+++ ComSclAppl/comsclappl_generator.py
@@ -37,7 +37,7 @@ class Generator:
             self.build_content()
             file.write(self.content)
         
-        print(f"[DEBUG] File existence: {os.path.join(self.dir, self.filename)}")
+        print(f"[DEBUG] File existence: {os.path.isfile(os.path.join(self.dir, self.filename))}")
 
     def build_header(self):
         """
